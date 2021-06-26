aa78cec293f7ccb1c72890123255509e51c744ef
vAlfred88 <valfred88@gmail.com>
2021-06-26 12:50:01 +0300

    dotenv installed

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').config();
+
 const ViberBot = require('viber-bot').Bot;
 const Event = require('viber-bot').Events;
 
@@ -14,14 +16,14 @@ bot.on(Event.MESSAGE_RECEIVED, (message, response) => {
 });
 
 const https = require('https');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
-// const webhookUrl = process.env.WEBHOOK_URL;
+const webhookUrl = process.env.WEBHOOK_URL;
 
-// const httpsOptions = {
+const httpsOptions = {
 //     key: ...,
 //     cert: ...,
 //     ca: ...
-// };
+};
 
 https.createServer(httpsOptions, bot.middleware()).listen(port, () => bot.setWebhook(webhookUrl));
